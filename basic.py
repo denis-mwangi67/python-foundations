9d8f41f9b75ed6370bf4646ebcda443c7659fc45
denis-mwangi67 <denismwangi3667@gmail.com>
2025-06-19 10:12:27 +0300

    indroduction to python

---
+++ basic.py
@@ -0,0 +1,37 @@
+# string manipulation
+#Add new line
+#rprint(" hello world\n hello world \n Hello world")
+
+#string concanting
+#print("Hello"+ " root")
+
+
+
+#input () fucntion
+
+#input("what is name: ")
+#print("Heloo " + " " + input("what is name: "))
+
+#quize
+
+#print("Hey"+ " " + input("what is name: ")+ "," + " how are you?")
+
+#python variable
+#name = input("what is name: ")
+#lenth =len(name)
+
+#print(f"your is {name} and is as a lenth of {lenth}")
+
+#quize swap 2 number
+
+a = input("enter value of a: ")
+b = input("enter value of b: ")
+temp = a
+
+#swap1 = a = b
+#swap2 = temp
+a, b = b, a
+print ("value of a: "+ a)
+print("value of b: "+ b)
+
+
